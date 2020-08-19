67eac03325332a6de7ceceaf6a3dc65acf39c074
Anastasia <arkadochnikova@gmail.com>
2020-08-19 01:16:14 +0100

    Gaussian mixture fix
    Prevent 0 weights in the mixture by artificially shifting the smallest weight to eps.

--- gm_elements.m
+++ gm_elements.m
@@ -1,9 +1,17 @@
 function[mixture] = gm_elements(x_s,P_s,mu_s)
+%%
+eps = 10e-8;
 [T,N] = size(x_s);
 for t =1:T
+    mu_mixture = mu_s{t};
     for j=1:N
         mean_mixute(:,j)    = x_s{t,j};  
         P_mixture(:,:,j)    = (P_s{t,j} + P_s{t,j}.')/2; 
+        %% mixture weights can't be zero - replace by an eps weight
+        if mu_mixture(j) < eps 
+            mu_mixture(j) = eps;
+        end
     end
-    mixture{t} = gmdistribution(mean_mixute',P_mixture,mu_s{t});
+    % matlab renormalises mixture weights in case they don't sum to one
+    mixture{t} = gmdistribution(mean_mixute',P_mixture,mu_mixture);
 end
